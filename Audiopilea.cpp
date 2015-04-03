2e1e05a134db079af7707e43fa7177e446071147
adisha94 <abetawar@gmail.com>
2015-04-02 17:01:01 -0700

    This is the initial commit for the file

---
+++ Audiopilea.cpp
@@ -0,0 +1,16 @@
+//
+//  Audiopilea.cpp
+//  Starta
+//
+//  Created by Adish Betawar on 4/2/15.
+//  Copyright (c) 2015 Adish Betawar. All rights reserved.
+//
+
+#include "Audiopilea.h"
+#include "EaseOfAccesss.h"
+
+int main()
+{
+    AudioPile aud;
+    
+}
---
+++ Audiopilea.h
@@ -0,0 +1,21 @@
+//
+//  Audiopilea.h
+//  Starta
+//
+//  Created by Adish Betawar on 4/2/15.
+//  Copyright (c) 2015 Adish Betawar. All rights reserved.
+//
+
+#ifndef __Starta__Audiopilea__
+#define __Starta__Audiopilea__
+
+
+class AudioPile
+{
+private:
+    int audioFile;
+public:
+    void ExecuteAudio();
+    void StopExecution();    
+};
+#endif /* defined(__Starta__Audiopilea__) */
